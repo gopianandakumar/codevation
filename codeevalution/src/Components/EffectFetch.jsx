183857b59c95d986881384231d5d1e7e209f816b
gopianandakumar <gopianandakumar@gmail.com>
2024-02-20 21:47:28 +0530

    get indivual user data using use effect

--- codeevalution/src/Components/EffectFetch.jsx
+++ codeevalution/src/Components/EffectFetch.jsx
@@ -3,20 +3,26 @@ import React, { useEffect, useState } from 'react'
 
 function EffectFetch() {
     const [posts, setPosts] = useState([])
+    const [id, setId] = useState(1)
 
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(res=>{
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res=>{
             setPosts(res.data)}
         ).catch(err=>{console.log(err)})
-    },[])
+    },[id])
   return (
-    <div>EffectFetch
-        <ul>{
+    <>
+        console.log(posts)
+        <input type='text' name='id' value={id} onChange={e=> setId(e.target.value)} /><br/>
+        {
+          posts.title
+        
+        /* <ul>{
             posts.map(pst =>(<li key={pst.id}>{pst.title}</li>))
             }
 
-        </ul>
-    </div>
+        </ul> */}
+    </>
   )
 }
 
